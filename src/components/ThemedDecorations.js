277a725f900d868b5164a1e8eb03f5e959d15135
Jason Vance <jbvance@gmail.com>
2017-12-24 13:26:49 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 class ThemedDecorations extends Component {
   const newChildren = React.Children.map(this.props.children, (child) => {
-    return React.cloneElement(child, {className: this.props.themeClass})
+    return React.cloneElement(child, {className: this.props.theme})
   });
 }
